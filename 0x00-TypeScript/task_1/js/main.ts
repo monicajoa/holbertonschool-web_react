7def2335fb0b21863e9c87a06415a465954f4701
monicajoa <mnortiz.ortiz@gmail.com>
2021-10-27 00:23:27 -0500

    Add Let's build a Teacher interface, 1. task

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,71 @@
+interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[newKey: string]: any;
+}
+
+const teacher3: Teacher = {
+	firstName: 'John',
+	fullTimeEmployee: false,
+	lastName: 'Doe',
+	location: 'London',
+	contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+const director1: Directors = {
+	firstName: 'John',
+	lastName: 'Doe',
+	location: 'London',
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+console.log(director1);
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
+	return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
+
+interface StudentClassInterface {
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+interface StudentClassConstructor {
+	new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+let StudentCls: StudentClassConstructor;
+StudentCls = class StudentClass implements StudentClassInterface {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return 'Currently working';
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
