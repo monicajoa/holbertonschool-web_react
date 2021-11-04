454dce59eb7bf634149c328b2379bb3eccb5cdeb
monicajoa <mnortiz.ortiz@gmail.com>
2021-11-03 23:46:23 -0500

    Add react-intro project, Install Babel task 9

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -30,13 +30,13 @@ module.exports = {
 					},
 				],
 			},
-			//{
-				//test: /\.(js|jsx)$/,
-        //exclude: /node_modules/,
-        //use: {
-          //loader: "babel-loader"
-        //},
-			//}
+			{
+				test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+			}
 		]
 	},
 
