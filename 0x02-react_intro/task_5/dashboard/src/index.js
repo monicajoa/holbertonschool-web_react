5a0fef478b64a40cf27635699170eabe42bed1f2
monicajoa <mnortiz.ortiz@gmail.com>
2021-11-03 23:52:17 -0500

    Add react-intro project, Reorganize the files task, 10

--- 0x02-react_intro/task_5/dashboard/src/index.js
+++ 0x02-react_intro/task_5/dashboard/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import Notifications from './Notifications/Notifications';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
