98a68692dd9ba8cdedf28e6cef4fd53bc648fe86
monicajoa <mnortiz.ortiz@gmail.com>
2022-01-06 21:53:37 -0500

    Add task - 8, Lazy Seq, change more than for less than, check Eslint

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,11 +2,11 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
   const studentsSeq = Seq(object);
-  let bestStudents = studentsSeq.filter((student) => student.score < 70);
+  let bestStudents = studentsSeq.filter((student) => student.score > 70);
   bestStudents = bestStudents.map((student) => {
     const capStudent = student;
-    capStudent.firstName = capStudent.firstName[0].toUpperCase() + capStudent.firstName.slice(1);
-    capStudent.lastName = capStudent.lastName[0].toUpperCase() + capStudent.lastName.slice(1);
+    capStudent.firstName = capStudent.firstName.charAt(0).toUpperCase() + capStudent.firstName.slice(1);
+    capStudent.lastName = capStudent.lastName.charAt(0).toUpperCase() + capStudent.lastName.slice(1);
     return capStudent;
   });
   bestStudents = bestStudents.toObject();
