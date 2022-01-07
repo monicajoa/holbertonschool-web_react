53f68ab1e56a7d4b7219571965962a40d57a0ca1
monicajoa <mnortiz.ortiz@gmail.com>
2022-01-06 19:51:09 -0500

    Add task 4 - Chained mutations

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -9,6 +9,6 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map.withMutations(index => {
+export const map2 = map.withMutations((index) => {
   index.set(2, 'Benjamin').set(4, 'Oliver');
 });
