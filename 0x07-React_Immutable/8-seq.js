466e5a9a3be4fab649754ae4418df6335d9862a5
monicajoa <mnortiz.ortiz@gmail.com>
2022-01-06 21:57:13 -0500

    Add task - 8, Lazy Seq, check Eslint

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -4,10 +4,10 @@ export default function printBestStudents(object) {
   const studentsSeq = Seq(object);
   let bestStudents = studentsSeq.filter((student) => student.score > 70);
   bestStudents = bestStudents.map((student) => {
-    const capStudent = student;
-    capStudent.firstName = capStudent.firstName.charAt(0).toUpperCase() + capStudent.firstName.slice(1);
-    capStudent.lastName = capStudent.lastName.charAt(0).toUpperCase() + capStudent.lastName.slice(1);
-    return capStudent;
+    const capStd = student;
+    capStd.firstName = capStd.firstName.charAt(0).toUpperCase() + capStd.firstName.slice(1);
+    capStd.lastName = capStd.lastName.charAt(0).toUpperCase() + capStd.lastName.slice(1);
+    return capStd;
   });
   bestStudents = bestStudents.toObject();
   console.log(bestStudents);
